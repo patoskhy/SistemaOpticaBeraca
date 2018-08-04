d5a8adafa3ef5be83d1a56170185cc738cd20d66
Patricio Aguilar <patricio.aguilar.v@hotmail.com>
2018-08-04 14:44:47 -0400

    cambios

---
+++ config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+	'zonaHoraria' => 'America/Santiago',
+	'servicioPaginaWeb' => 'http://www.opticaberaca.cl/sistemaWS/servicio.php?wsdl',
+	'nombreSistema' => strtoupper('Óptica Beraca')
+];
